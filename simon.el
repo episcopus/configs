bceb12213bd1d086209a1592f16030b5700e3ebb
episcopus <episcopus@users.noreply.github.com>
2016-11-29 22:19:03 -0800

    Disable flycheck mode and simplify font decoration for performance of editing.

--- simon.el
+++ simon.el
@@ -6,8 +6,11 @@
 (defun my-c-mode-common-hook ()
   ;; my customizations for all of c-mode and related modes
   (linum-mode)
+  (flycheck-mode -1)
   )
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(setq font-lock-maximum-decoration '((c-mode . 2) (c++-mode . 2)))
 
 (provide 'simon)
 ;;; simon.el ends here
